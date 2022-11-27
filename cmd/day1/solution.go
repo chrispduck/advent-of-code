93ddb1c4eed449537f766de0e0c4b188363395a8
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2022-11-27 12:29:54 +0000

    chore: template solution

---
+++ cmd/day1/solution.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	arg := 0
+	fmt.Println("arg: ", arg)
+	fmt.Println("solution: ", solution(0))
+	return
+}
+
+func solution(arg int) int {
+	return 0
+}
