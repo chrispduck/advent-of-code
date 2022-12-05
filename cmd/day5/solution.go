cfa8227c9afa092b5b001938c8c22b84f766694b
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2022-12-05 11:46:21 +0000

    parta generalised

--- cmd/day5/solution.go
+++ cmd/day5/solution.go
@@ -5,51 +5,73 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(part1("example_input.txt"))
-	fmt.Println(part1("input.txt"))
+	fmt.Println(part1("example_input.txt", 3))
+	fmt.Println(part1("input.txt", 9))
 }
 
-func part1(filename string) string {
+func part1(filename string, n int) string {
 	f, err := os.Open(filename)
 	checkErr(err)
 	scanner := bufio.NewScanner(f)
 
-	var stackA, stackB, stackC []rune
+	stacks := make([][]rune, n)
 	var instructions [][]int
 	stateRead := false
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		//fmt.Println(line)
 		if (line == "") || (line[1] == '1') {
 			// deal with split between state input and instructions
 			//fmt.Println("now reading instructions")
 			stateRead = true
 		} else {
 			if !stateRead {
-				if line[1] != ' ' {
-					prepend(&stackA, rune(line[1]))
-				}
-				if len(line) >= 6 && line[5] != ' ' {
-					prepend(&stackB, rune(line[5]))
-				}
-				if len(line) >= 10 && line[9] != ' ' {
-					prepend(&stackC, rune(line[9]))
-				}
-				// prepare stacks
-				//fmt.Printf("stackA %c\n", stackA)
-				//fmt.Printf("stackB %c\n", stackB)
-				//fmt.Printf("stackC %c\n", stackC)
+				//fmt.Println("parsing input")
+
+				parseInputState(&stacks, line, n)
+				//fmt.Printf("stacks main: %+c\n", stacks)
+
 			} else {
-				instructions = append(instructions, []int{int(line[5] - '0'), int(line[12] - '0'), int(line[17] - '0')})
+				instructions = append(instructions, parseInstruction(line))
 			}
 		}
 	}
 	checkErr(scanner.Err())
-	stackA, stackB, stackC = execute(instructions, stackA, stackB, stackC)
-	return getTopCrates(stackA, stackB, stackC)
+	//fmt.Println(instructions)
+	execute(instructions, &stacks)
+	//fmt.Printf("stacks main: %+c\n", stacks)
+	return getTopCrates(&stacks)
+}
+
+func parseInputState(stacks *[][]rune, line string, n int) {
+	for i := 0; i < n; i++ {
+		idx := 4*i + 1
+		if len(line) < idx {
+			return
+		}
+		if line[idx] != ' ' {
+			//fmt.Printf("input letter: %c", line[idx])
+			prepend(&(*stacks)[i], rune(line[idx]))
+		}
+	}
+	//fmt.Printf("%c\n, %c\n, %c\n", (*stacks)[0], (*stacks)[1], (*stacks)[2])
+}
+
+func parseInstruction(line string) []int {
+	rx := regexp.MustCompile(`\d+`)
+	s := rx.FindAllString(line, -1)
+	return []int{strToInt(s[0]), strToInt(s[1]), strToInt(s[2])}
+}
+
+func strToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	checkErr(err)
+	return i
 }
 
 func checkErr(err error) {
@@ -63,7 +85,6 @@ func pop(arr *[]rune) rune {
 	res := (*arr)[len(*arr)-1]
 	*arr = (*arr)[:len(*arr)-1]
 	//fmt.Printf("stack pop %c, item %c\n", arr, res)
-
 	return res
 }
 
@@ -75,26 +96,23 @@ func prepend(arr *[]rune, item rune) {
 	*arr = append([]rune{item}, *arr...)
 }
 
-func execute(instructions [][]int, stackA, stackB, stackC []rune) ([]rune, []rune, []rune) {
-	stacks := []*[]rune{&stackA, &stackB, &stackC}
+func execute(instructions [][]int, stacks *[][]rune) {
 	for _, instruction := range instructions {
 		repeats, from, to := instruction[0], instruction[1], instruction[2]
+		//fmt.Printf("stack state: %+c\n", stacks)
 		//fmt.Println(repeats, from, to)
 		for i := 0; i < repeats; i++ {
-			item := pop(stacks[from-1])
-			push(stacks[to-1], item)
-			//fmt.Printf("stackA %c\n", stackA)
-			//fmt.Printf("stackB %c\n", stackB)
-			//fmt.Printf("stackC %c\n", stackC)
+			item := pop(&(*stacks)[from-1])
+			push(&(*stacks)[to-1], item)
 		}
-
 	}
-	return *stacks[0], *stacks[1], *stacks[2]
 }
 
-func getTopCrates(stackA, stackB, stackC []rune) string {
-	s1 := string(pop(&stackA))
-	s2 := string(pop(&stackB))
-	s3 := string(pop(&stackC))
-	return s1 + s2 + s3
+func getTopCrates(stacks *[][]rune) string {
+	res := ""
+	for i := 0; i < len(*stacks); i++ {
+		res += string((*stacks)[i][len((*stacks)[i])-1])
+
+	}
+	return res
 }
