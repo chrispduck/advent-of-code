4f800bb26bebbef304f7663e9f759a6f548e9332
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2023-03-24 17:56:57 +0000

    tidy: run cargofmt

--- 2021/solutions/src/bin/day1.rs
+++ 2021/solutions/src/bin/day1.rs
@@ -10,14 +10,13 @@ fn main() {
     println!("part1 : {}", part1(&v));
     println!("part2 : {}", part2(&v));
 
-
     let fname = "data/day1/input.txt";
     let v = load_input(fname);
     println!("part1 : {}", part1(&v));
     println!("part2 : {}", part2(&v));
 }
 
-fn load_input(fname: &str)-> Vec<i32>{
+fn load_input(fname: &str) -> Vec<i32> {
     let data_path = Path::new(fname);
     // println!("current_dir: {:?}", current_dir());
     let mut file = File::open(data_path).unwrap();
@@ -25,13 +24,11 @@ fn load_input(fname: &str)-> Vec<i32>{
     file.read_to_string(&mut contents).unwrap();
     // println!("contents: {}", contents);
 
-
     // Using collect to create a vector
     let _v: Vec<i32> = contents
         .lines()
         .map(|s| s.parse::<i32>().unwrap())
         .collect();
-
 
     // Another way to create a vector
     let mut v2: Vec<i32> = vec![];
@@ -41,14 +38,14 @@ fn load_input(fname: &str)-> Vec<i32>{
     }
     // println!("v: {:?}", v);
 
-    return v2
+    return v2;
 }
 
 fn part1(v: &Vec<i32>) -> i32 {
     let mut sum = 0;
     for i in 1..v.len() {
-        if v[i-1] < v[i]{
-            sum+=1;
+        if v[i - 1] < v[i] {
+            sum += 1;
         }
     }
     return sum;
@@ -56,13 +53,13 @@ fn part1(v: &Vec<i32>) -> i32 {
 
 fn part2(v: &Vec<i32>) -> i32 {
     let mut sum = 0;
-    for i in 1..(v.len()-2) {
-        let lhs = v[i-1] + v[i] + v[i+1];
-        let rhs = v [i] + v[i+1] + v[i+2];
+    for i in 1..(v.len() - 2) {
+        let lhs = v[i - 1] + v[i] + v[i + 1];
+        let rhs = v[i] + v[i + 1] + v[i + 2];
         if lhs < rhs {
-            sum+=1;
+            sum += 1;
         }
         // println!("{}", i)
     }
-    return sum
+    return sum;
 }
