2eb5293ab2883820a50925ba109875d071ab9bac
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2023-05-07 13:36:20 +0100

    feat: complete day7

--- 2021/solutions/src/bin/day7.rs
+++ 2021/solutions/src/bin/day7.rs
@@ -6,16 +6,15 @@ use std::path::Path;
 use num_complex::Complex;
 
 fn main() {
-    // TODO change to day number
     let input_dir = Path::new("data/day7/");
 
     let data = load_input(input_dir.join("example_input.txt").to_str().unwrap());
     println!("part1 : {}", part1(&data));
-    // println!("part2 : {}", part2(data));
-    //
+    println!("part2 : {}", part2(&data));
+
     let data = load_input(input_dir.join("input.txt").to_str().unwrap());
     println!("part1 : {}", part1(&data));
-    // println!("part2 : {}", part2(data));
+    println!("part2 : {}", part2(&data));
 }
 
 fn load_input(fname: &str) -> Vec<i64> {
@@ -36,13 +35,21 @@ fn part1(v: &Vec<i64>) -> i64 {
     let mut res = i64::MAX;
     for pos in min..max + 1 {
         let fuel: i64 = v.iter().map(|x| (x - pos).abs()).sum();
-        println!("pos: {}, fuel: {}", pos, fuel);
         res = res.min(fuel);
     }
     return res;
 }
 
-fn part2(v: Vec<u64>) -> u64 {
-    // TODO implement
-    return 0;
+fn part2(v: &Vec<i64>) -> i64 {
+    let min: i64 = v.iter().min().unwrap().clone();
+    let max: i64 = v.iter().max().unwrap().clone();
+    let mut res = i64::MAX;
+    for pos in min..max + 1 {
+        let fuel: i64 = v
+            .iter()
+            .map(|x| (((x - pos).abs() * ((x - pos).abs() + 1)) / 2))
+            .sum();
+        res = res.min(fuel);
+    }
+    return res;
 }
